9b8c5f3e93ce8b4ced112ab01b58f78c60ac902b
harshith <harshithkalki@gmail.com>
2022-10-03 23:08:50 +0530

    nav finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from './components/Header';
+import HomePage from "./Pages/HomePage";
+import CoinPage from "./Pages/CoinPage";
+// import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} exact />
+          <Route path="/coins/:id" element={<CoinPage />} />
+        </Routes>
+      </div >
+    </Router>
   );
 }
 
