c31b3f28336b6923f78823b484ebbbfb38d986f8
harshith <harshithkalki@gmail.com>
2022-10-08 23:20:28 +0530

    theme- blue-dark

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <ColorModeScript initialColorMode='light'></ColorModeScript>
+      <ColorModeScript initialColorMode='dark'></ColorModeScript>
       <CryptoContext>
         <App />
       </CryptoContext>
